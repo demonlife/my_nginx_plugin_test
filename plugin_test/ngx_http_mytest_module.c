b5813c67820e019408e467386bc412f9adb8c64f
demon <demonlife2013@gmail.com>
2014-12-01 09:47:05 +0800

    add setting my context

--- plugin_test/ngx_http_mytest_module.c
+++ plugin_test/ngx_http_mytest_module.c
@@ -2,85 +2,206 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-static char* ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r);
+static void* ngx_http_mytest_create_loc_conf(ngx_conf_t *cf);
 
-static ngx_http_module_t ngx_http_mytest_module_ctx = {
-    NULL, NULL,
-    NULL, NULL,
-    NULL, NULL,
-    NULL, NULL
+static char* ngx_conf_set_myconfig(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
+typedef struct {
+    // 以下14个定义使用Nginx预定义的方法
+    ngx_str_t   	my_str;
+    ngx_int_t   	my_num;
+    ngx_flag_t   	my_flag;
+    size_t		my_size;
+    ngx_array_t*  	my_str_array;
+    ngx_array_t*  	my_keyval;
+    off_t   	my_off;
+    ngx_msec_t   	my_msec;
+    time_t   	my_sec;
+    ngx_bufs_t   	my_bufs;
+    ngx_uint_t   	my_enum_seq;
+    ngx_uint_t	my_bitmask;
+    ngx_uint_t   	my_access;
+    ngx_path_t*	my_path;
+
+    // 使用自定义的方法处理参数
+    ngx_str_t		my_config_str;
+    ngx_int_t		my_config_num;
+} ngx_http_mytest_conf_t;
+
+static ngx_conf_enum_t test_enums[] = {
+    {ngx_string("apple"), 1},
+    {ngx_string("banana"), 2},
+    {ngx_string("orange"), 3},
+    {ngx_null_string, 0}
+};
+
+static ngx_conf_bitmask_t test_bitmasks[] = {
+    {ngx_string("good"), 0x0002},
+    {ngx_string("better"), 0x0004},
+    {ngx_string("best"), 0x0008},
+    {ngx_null_string, 0}
 };
 
 static ngx_command_t ngx_http_mytest_commands[] = {
     {
-        ngx_string("mytest"),
-        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF |
-        NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-        ngx_http_mytest,
+        ngx_string("test_flag"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_FLAG,
+        ngx_conf_set_flag_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_flag),
+        NULL
+    },
+
+    {
+        ngx_string("test_str"),
+        NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_str_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_str),
+        NULL
+    },
+
+    {
+        ngx_string("test_str_array"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_str_array_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_str_array),
+        NULL
+    },
+
+    {
+        ngx_string("test_keyval"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE2,
+        ngx_conf_set_keyval_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_keyval),
+        NULL
+    },
+
+    {
+        ngx_string("test_num"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_num_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_num),
+        NULL
+    },
+
+    {
+        ngx_string("test_size"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_size_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_size),
+        NULL
+    },
+
+    {
+        ngx_string("test_off"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_off_slot, NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_off),
+        NULL
+    },
+
+    {
+        ngx_string("test_msec"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_msec_slot, NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_msec),
+        NULL
+    },
+
+    {
+        ngx_string("test_sec"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_sec_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_sec),
+        NULL
+    },
+
+    {
+        ngx_string("test_bufs"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE2,
+        ngx_conf_set_bufs_slot, NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_bufs),
+        NULL
+    },
+
+    {
+        ngx_string("test_enum"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_enum_slot, NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_enum_seq),
+        test_enums
+    },
+
+    {
+        ngx_string("test_bitmask"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_bitmask_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_bitmask),
+        test_bitmasks
+    },
+
+    {
+        ngx_string("test_access"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE123,
+        ngx_conf_set_access_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_access),
+        NULL
+    },
+
+    {
+        ngx_string("test_path"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1234,
+        ngx_conf_set_path_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_mytest_conf_t, my_path),
+        NULL
+    },
+
+    {
+        ngx_string("test_myconfig"),
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE12,
+        ngx_conf_set_myconfig,
         NGX_HTTP_LOC_CONF_OFFSET,
         0,
         NULL
     },
+
+
     ngx_null_command
+
 };
 
-ngx_module_t ngx_http_mytest_module = {
-    NGX_MODULE_V1,
-    &ngx_http_mytest_module_ctx,
-    ngx_http_mytest_commands,
-    NGX_HTTP_MODULE,
-    NULL, NULL, NULL, NULL,
-    NULL, NULL, NULL,
-    NGX_MODULE_V1_PADDING
-};
-
-static char* ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
-    ngx_http_core_loc_conf_t *clcf;
-
-    // 找到mytest配置项所属的配置块，clcf可以是main,srv,loc级别配置项，即
-    // 在每个http{}, server{}内都有一个ngx_http_core_loc_conf_t结构体
-    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-
-    // http框架在处理用户请求进行到NGX_HTTP_CONTENT_PHASE阶段时，如果请求的主机
-    // 域名，URI与mytest配置项所在的配置块相匹配，就调用该方法
-    clcf->handler = ngx_http_mytest_handler;
-    
-    return NGX_CONF_OK;
+static char* ngx_conf_set_myconfig(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+    // 参数conf就是http框架传给我们的，在ngx_http_mytest_create_loc_conf回调方法中
+    // 分配的结构体体ngx_http_mytest_conf_t
+    // void* 赋值给任何指针都不需要强制转换
+    ngx_http_mytest_conf_t *mycf = conf;
 }
 
-static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r) {
-    if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))) {
-        return NGX_HTTP_NOT_ALLOWED;
+static void *ngx_http_mytest_create_loc_conf(ngx_conf_t *cf) {
+    ngx_http_mytest_conf_t *mycf;
+    mycf = (ngx_http_mytest_conf_t*)ngx_pcalloc(cf->pool, sizeof(ngx_http_mytest_conf_t));
+    if (mycf == NULL) {
+        return NULL;
     }
 
-    ngx_int_t rc = ngx_http_discard_request_body(r);
-    if (rc != NGX_OK) {
-        return rc;
-    }
+    // 为配置参数设置初始值， 为nginx定义的14个方法做准备
+    mycf->my_flag = NGX_CONF_UNSET;
+    mycf->my_num = NGX_CONF_UNSET;
+    mycf->my_str_array = NGX_CONF_UNSET_PTR;
+    mycf->my_keyval = NULL;
+    mycf->my_off = NGX_CONF_UNSET;
+    mycf->my_msec = NGX_CONF_UNSET_MSEC;
+    mycf->my_sec = NGX_CONF_UNSET;
+    mycf->my_size = NGX_CONF_UNSET_SIZE;
 
-    ngx_str_t type = ngx_string("text/plain");
-    ngx_str_t response = ngx_string("Hello world!");
-    r->headers_out.status = NGX_HTTP_OK;
-    r->headers_out.content_length_n = response.len;
-    r->headers_out.content_type = type;
-
-    rc = ngx_http_send_header(r);
-    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
-        return rc;
-    }
-
-    ngx_buf_t *b;
-    b = ngx_create_temp_buf(r->pool, response.len);
-    if (b == NULL) {
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
-    ngx_memcpy(b->pos, response.data, response.len);
-    b->last = b->pos + response.len;
-    b->last_buf = 1;
-    ngx_chain_t out;
-    out.buf = b;
-    out.next = NULL;
-
-    return ngx_http_output_filter(r, &out);
+    return mycf;
 }
